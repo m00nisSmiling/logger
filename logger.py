b704b91a00841d5c55ee3e7589c0747f476ad775
moonissmiling <122709554+m00nisSmiling@users.noreply.github.com>
2025-09-22 16:29:14 +0700

    Update logger.py

--- logger.py
+++ logger.py
@@ -12,6 +12,7 @@ else:
     root = Tk()
     root.configure(bg="black")
     root.title("Logger")
+
     frame1 = Frame(root)
     frame1.configure(bg="black",width=800,height=800)
 
@@ -34,6 +35,9 @@ else:
 
     issueTitle = Label(frame1, text="Issue Title  ", fg="lime", bg="black", font=("Aerial",15,"bold"))
     issueTitleEnt = Entry(frame1, textvariable=issueTitleVar, fg="cyan", bg="black", width=100, font=("Aerial",10,"bold"), highlightcolor="blue")
+
+    riskLabel = Label(frame1, text="Risk Rating ", fg="lime", bg="black", font=("Aerial",15,"bold"))
+    riskComboBox = ttk.Combobox(frame1, values=["","Low","Medium","High","Critical"], width=10)
 
     vulnUrl = Label(frame1, text="Endpoint  ", fg="lime", bg="black", font=("Aerial",15,"bold"))
     vulnUrlEnt = Entry(frame1, textvariable=vulnUrlVar, fg="cyan", bg="black", width=100, font=("Aerial",10,"bold"), highlightcolor="blue")
@@ -63,6 +67,7 @@ else:
         sIssueNo = issueEnt.get()
         sTitle = issueTitleVar.get()
         sPj = pjVar.get()
+        sRisk = riskComboBox.get()
         sMethod = methodComboBox.get()
         sVulnUrl = vulnUrlVar.get()
         sData = dataEnt.get(0.0,END).strip()
@@ -70,7 +75,7 @@ else:
         sDay = daySpinBox.get()
         sMonth = monthSpinBox.get()
         sYear = yearSpinBox.get()
-        dataText = f"#{sPj}_issueNo_{sIssueNo}\n{sTitle}\nVulnerable endpoint : {sVulnUrl}\nHTTP method         : {sMethod}\nRequest data        : {sData}\nImpact              : {sImpact}\nDate                : {sDay}/{sMonth}/{sYear}\n----------------------------------------------------------------------\n"
+        dataText = f"#{sPj}_issueNo_{sIssueNo}\n{sTitle}\nRisk rating         : {sRisk}\nVulnerable endpoint : {sVulnUrl}\nHTTP method         : {sMethod}\nRequest data        : {sData}\nImpact              : {sImpact}\nDate                : {sDay}/{sMonth}/{sYear}\n----------------------------------------------------------------------\n"
         logfile = open(f"{filei}","a")
         logfile.write(dataText)
         impactEnt.delete(0,END)
@@ -81,7 +86,7 @@ else:
 
     writeButton = Button(frame1, text="Write", fg="white", bg="black", font=("Aerial",20,"bold"), width=10, height=2, command=add, activebackground="lime", activeforeground="black")
 
-    frame1.pack(pady=80,side=LEFT,padx=10)
+    frame1.pack(pady=50,side=LEFT,padx=10)
     frame2.pack(pady=80,side=RIGHT,padx=30)
     titleLabel.grid( row=0, column=0, columnspan=2, padx=5)
     pjLabel.grid( row=1, column=0, sticky="e")
@@ -90,21 +95,23 @@ else:
     issueEnt.grid( row=2, column=1, sticky="w")
     issueTitle.grid( row=3, column=0, sticky="e")
     issueTitleEnt.grid( row=3, column=1, sticky="w")
-    vulnUrl.grid( row=4, column=0, sticky="e")
-    vulnUrlEnt.grid( row=4, column=1, sticky="w")
-    methodLabel.grid( row=5, column=0, sticky="e")
-    methodComboBox.grid( row=5, column=1, sticky="w")
-    dataLabel.grid( row=6, column=0, sticky="e")
-    dataEnt.grid( row=6, column=1, sticky="w")
-    impactLabel.grid( row=7, column=0, sticky="e")
-    impactEnt.grid( row=7, column=1, sticky="w")
-    dayLabel.grid( row=8, column=0, sticky="e")
-    daySpinBox.grid( row=8, column=1, sticky="w")
-    monthLabel.grid( row=9, column=0, sticky="e")
-    monthSpinBox.grid( row=9, column=1, sticky="w")
-    yearLabel.grid( row=10, column=0, sticky="e")
-    yearSpinBox.grid( row=10, column=1, pady=10, sticky="w")
-    writeButton.grid( row=11, column=0, columnspan=2)
+    riskLabel.grid( row=4, column=0, sticky="e")
+    riskComboBox.grid( row=4, column=1, sticky="w")
+    vulnUrl.grid( row=5, column=0, sticky="e")
+    vulnUrlEnt.grid( row=5, column=1, sticky="w")
+    methodLabel.grid( row=6, column=0, sticky="e")
+    methodComboBox.grid( row=6, column=1, sticky="w")
+    dataLabel.grid( row=7, column=0, sticky="e")
+    dataEnt.grid( row=7, column=1, sticky="w")
+    impactLabel.grid( row=8, column=0, sticky="e")
+    impactEnt.grid( row=8, column=1, sticky="w")
+    dayLabel.grid( row=9, column=0, sticky="e")
+    daySpinBox.grid( row=9, column=1, sticky="w")
+    monthLabel.grid( row=10, column=0, sticky="e")
+    monthSpinBox.grid( row=10, column=1, sticky="w")
+    yearLabel.grid( row=11, column=0, sticky="e")
+    yearSpinBox.grid( row=11, column=1, pady=10, sticky="w")
+    writeButton.grid( row=12, column=0, columnspan=2)
     resultLabel.grid( row=0, column=0, pady=10)
     resultText.grid( row=1, column=0, pady=10)
 
